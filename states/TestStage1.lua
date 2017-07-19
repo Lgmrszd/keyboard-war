c597ec1c89c2a4239ad6a6a149cbcc8768b16f0f
Penguinum <penguinum@gmail.com>
2017-07-19 17:59:19 +0300

    More enemies

--- states/TestStage1.lua
+++ states/TestStage1.lua
@@ -4,31 +4,56 @@ local Bullet
 Bullet = require("lib.Bullet").Bullet
 local lovelog = require("lib.lovelog")
 local config = require("config")
+local enemies = {
+  simple1 = {
+    pos = Vector(5, 10),
+    move = function(self, dt)
+      self.pos = self.pos + 200 * Vector(1, 1) * dt
+    end,
+    shoot = function(self)
+      return Bullet({
+        pos = self.pos + Vector(0, 10),
+        speed = math.random(50, 100),
+        dir = Vector(0.2 * (math.random() - 0.5), math.random()):normalized(),
+        char = "*"
+      })
+    end
+  },
+  simple2 = {
+    pos = Vector(595, 10),
+    move = function(self, dt)
+      self.pos = self.pos + 200 * Vector(-1, 1) * dt
+    end,
+    shoot = function(self)
+      return Bullet({
+        pos = self.pos + Vector(0, 10),
+        speed = math.random(50, 100),
+        dir = Vector(0.2 * (math.random() - 0.5), math.random()):normalized(),
+        char = "*"
+      })
+    end
+  }
+}
 local Stage1
 do
   local _class_0
   local enemy
   local _base_0 = {
     enter = function(self)
+      self.time = 0
       love.graphics.setFont(config.fonts.art)
       SceneManager:spawnPlayer(Vector(0.5, 0.9))
-      return SceneManager:spawnEnemy({
-        pos = Vector(0.5, 0.5),
-        move = function(self, dt)
-          self.pos = self.pos + 200 * Vector(1, 1) * dt
-        end,
-        shoot = function(self)
-          return Bullet({
-            pos = self.pos + Vector(0, 10),
-            speed = math.random(50, 100),
-            dir = Vector(0.2 * (math.random() - 0.5), math.random()):normalized(),
-            char = "*"
-          })
-        end
-      })
+      SceneManager:spawnEnemy(enemies.simple1)
+      return SceneManager:spawnEnemy(enemies.simple2)
     end,
     update = function(self, dt)
-      return SceneManager:update(dt)
+      SceneManager:update(dt)
+      self.time = self.time + dt
+      if self.time > 3 and (not self.spawned) then
+        self.spawned = true
+        SceneManager:spawnEnemy(enemies.simple1)
+        return SceneManager:spawnEnemy(enemies.simple2)
+      end
     end,
     draw = function(self)
       lovelog.reset()
--- states/TestStage1.moon
+++ states/TestStage1.moon
@@ -4,29 +4,52 @@ import Bullet from require "lib.Bullet"
 lovelog = require "lib.lovelog"
 config = require "config"
 
+enemies = {
+  simple1: {
+    pos: Vector(5, 10) -- Too bad we have to use px coords atm. FIXME
+    move: (dt) =>
+      @pos = @pos + 200 * Vector(1, 1) * dt
+    shoot: =>
+      Bullet{
+        pos: @pos + Vector(0, 10)
+        speed: math.random(50, 100)
+        dir: Vector(0.2*(math.random!-0.5), math.random!)\normalized!
+        char: "*"
+      }
+  }
+  simple2: {
+    pos: Vector(595, 10) -- FIXME
+    move: (dt) =>
+      @pos = @pos + 200 * Vector(-1, 1) * dt
+    shoot: =>
+      Bullet{
+        pos: @pos + Vector(0, 10)
+        speed: math.random(50, 100)
+        dir: Vector(0.2*(math.random!-0.5), math.random!)\normalized!
+        char: "*"
+      }
+  }
+}
+
 class Stage1
   -- canvas = love.graphics.newCanvas love.graphics.getWidth! - 200, love.graphics.getHeight!
   enemy = nil
 
   enter: =>
+    @time = 0
     love.graphics.setFont config.fonts.art
     SceneManager\spawnPlayer Vector(0.5, 0.9)
-    SceneManager\spawnEnemy{
-      pos: Vector(0.5, 0.5)
-      move: (dt) =>
-        @pos = @pos + 200 * Vector(1, 1) * dt
-      shoot: =>
-        Bullet{
-          pos: @pos + Vector(0, 10)
-          speed: math.random(50, 100)
-          dir: Vector(0.2*(math.random!-0.5), math.random!)\normalized!
-          char: "*"
-        }
-    }
+    SceneManager\spawnEnemy enemies.simple1
+    SceneManager\spawnEnemy enemies.simple2
     -- SceneManager\spawnBoss Vector(0.5, 0.05)
 
   update: (dt) =>
     SceneManager\update dt
+    @time += dt
+    if @time > 3 and (not @spawned)
+      @spawned = true
+      SceneManager\spawnEnemy enemies.simple1
+      SceneManager\spawnEnemy enemies.simple2
 
   draw: =>
     lovelog.reset!
